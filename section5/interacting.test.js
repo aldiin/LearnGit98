f4f2d9a0b4b9393d8f7fa801b91ae41e25d36108
aldiin <aldiindrawan98@gmail.com>
2022-09-18 12:10:12 +0700

    Merge branch 'master' of https://github.com/aldiin/LearnGit98

---
+++ section5/interacting.test.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+    it('Visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url('include', 'login.html')
+    });
+    it('should fill username', () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')    
+    });
+    it('should fill password', () => {
+        cy.get('input[name="user_password"]').clear()
+        cy.get('input[name="user_password"]').type('password')
+    });
+    it('should fill checkbox', () => {
+        cy.get('[type="checkbox"]').check()
+        cy.get('[type="checkbox"]').first().check()
+    });
+});
