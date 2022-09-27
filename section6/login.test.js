365b20bbccf4c95f67615101e48dedb6881609af
aldiin <aldiindrawan98@gmail.com>
2022-09-27 16:46:44 +0700

    cypress

--- section6/login.test.js
+++ section6/login.test.js
@@ -1,40 +1,41 @@
 /// <reference types="cypress" />
 
-describe('Login / Logout Test', () =>{
-    before(() => {
-        cy.visit('http://zero.webappsecurity.com/index.html')
-        cy.url().should('include','index.html')
-        cy.get('#signin_button').click()
+describe("Login / Logout Test", () => {
+  before(() => {
+    cy.visit("http://zero.webappsecurity.com/index.html");
+    cy.url().should("include", "index.html");
+    cy.get("#signin_button").click();
+  });
+
+  it("Should try to login with invalid data", () => {
+    cy.get("#login_form").should("be.visible");
+    cy.get("#user_login").type("invalid username");
+    cy.get("#user_password").type("invalid password");
+    cy.get("input[name=submit]").click();
+  });
+
+  it("Should display error message", () => {
+    cy.get(".alert-error")
+      .should("be.visible")
+      .and("contains.text", "Login and/or password are wrong.");
+  });
+
+  it("Should try to login with valid data", () => {
+    cy.fixture("user").then((user) => {
+      const username = user.username;
+      const password = user.password;
+
+      cy.login(username, password);
+
+      cy.get("h2").should("contain.text", "Cash Accounts");
     });
+  });
 
-    it('Should try to login with invalid data', () => {
-        cy.get('#login_form').should('be.visible')
-        cy.get('#user_login').type('invalid username')
-        cy.get('#user_password').type('invalid password')
-        cy.get('input[name=submit]').click()
-    });
-
-    it('Should display error message', () => {
-        cy.get('.alert-error').should('be.visible').and('contains.text', 'Login and/or password are wrong.')
-    });
-
-    it('Should try to login with valid data', () => {
-        cy.fixture("user").then(user =>{
-            const username = user.username
-            const password = user.password
-
-            cy.login(username,password)
-
-            cy.get('h2').should('contain.text', 'Cash Accounts')
-        })
-        
-    });
-
-    it('Should logout from the application', () => {
-        cy.logout()
-		cy.get('strong').should('contain.text', 'Home')
-		cy.url().should('equal', 'http://zero.webappsecurity.com/index.html')
-    });
+  it("Should logout from the application", () => {
+    cy.logout();
+    cy.get("strong").should("contain.text", "Home");
+    cy.url().should("equal", "http://zero.webappsecurity.com/index.html");
+  });
 });
 
 // isi file Commands.js
@@ -46,4 +47,4 @@ describe('Login / Logout Test', () =>{
 //     cy.get('input[name=submit]').click()
 
 //     cy.get('h2').should('contain.text', 'Cash Accounts')
-// }) 
+// })
