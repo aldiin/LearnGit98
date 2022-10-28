00203aaac0db8587fddea94a49de5caf19f968f0
aldiin <aldiindrawan98@gmail.com>
2022-10-28 18:08:04 +0700

    API

--- section9/content.test.js
+++ section9/content.test.js
@@ -10,10 +10,7 @@ describe("Validate Content", () => {
         }
       ]
     }
-    cy.request("GET", "https://pokeapi.co/api/v2/pokemon/ditto", pokemon).then(
-      (response) => {
-        expect(response.body.abilities[0].ability.name).to.eq(pokemon.abilities[0].ability.name)
-      }
-    )
+        expect(pokemon.abilities[0].ability.url).to.eq(pokemon.abilities[0].ability.url) 
+        expect(pokemon.abilities[0].ability.name).to.eq(pokemon.abilities[0].ability.name) 
   })
 })
